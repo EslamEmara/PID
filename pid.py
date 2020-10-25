30c2f8455320254c6427194a14edb6911d893ce7
enmo2sh <48364892+enmo2sh@users.noreply.github.com>
2020-10-25 15:33:07 +0400

    mpu module

--- pid.py
+++ pid.py
@@ -1,6 +1,4 @@
 import time
-import busio
-from Adafruit_BNO055 import BNO055
 
 # pid constants
 kd = 0.5
@@ -34,6 +32,34 @@ depth_desired_value = 0         # change its value when the rov goes up or down
 depth_old_error = 0
 depth_current_error = 0
 depth_error_integral = 0
+
+sent = False
+def server_recieve():
+    global sent
+    if sent == False:
+        sent = True
+        return True
+    else:
+        return False
+
+def get_data_test():
+    roll = int(input('current imu roll reading='))
+    rotate =int(input('current imu rotate reading='))
+    pitch = int(input('current imu pitch reading='))
+    return roll,pitch,rotate
+
+
+def imu_current_reading():
+    #roll,pitch,rotate = imu.get_gyro_data()
+    roll,pitch,rotate = get_data_test()
+
+    return roll,pitch,rotate
+
+def get_desired_location():
+    if (server_recieve()):
+        return imu_current_reading()
+    else:
+        return False
 
 # speeds list of motors to change their speed
 def calculate_pid_error(speeds,current_error,old_error,current_integral):
@@ -89,40 +115,7 @@ def calculate_depth_error(self):
     depth_error_integral = calculate_pid_error(speeds, depth_current_error, depth_old_error, depth_error_integral)
 
 
-# Create and configure the BNO sensor connection.  Make sure only ONE of the
-# below 'bno = ...' lines is uncommented:
-# Raspberry Pi configuration with serial UART and RST connected to GPIO 18:
-# imu = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
-# BeagleBone Black configuration with default I2C connection (SCL=P9_19, SDA=P9_20),
-# and RST connected to pin P9_12:
-# imu= BNO055.BNO055(rst='P9_12')
-
-
-
 while True:
-    # Read the Euler angles for heading, roll, pitch (all in degrees).
-    # heading, roll, pitch = imu.read_euler()
-    # Print everything out.
-    # print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(heading, roll, pitch, sys, gyro, accel, mag))
-    # Other values you can optionally read:
-    # Orientation as a quaternion:
-    # x,y,z,w = imu.read_quaterion()
-    # Sensor temperature in degrees Celsius:
-    # temp_c = imu.read_temp()
-    # Magnetometer data (in micro-Teslas):
-    # x,y,z = imu.read_magnetometer()
-    # Gyroscope data (in degrees per second):
-    # x,y,z = imu.read_gyroscope()
-    # Accelerometer data (in meters per second squared):
-    # x,y,z = imu.read_accelerometer()
-    # Linear acceleration data (i.e. acceleration from movement, not gravity--
-    # returned in meters per second squared):
-    # x,y,z = imu.read_linear_acceleration()
-    # Gravity acceleration data (i.e. acceleration just from gravity--returned
-    # in meters per second squared):
-    # x,y,z = imu.read_gravity()
-    # Sleep for a second until the next reading.
-    # time.sleep(1)
 
     roll_desired_value,pitch_desired_value,rotate_desired_value=get_desired_location()
 
